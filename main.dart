fd99c8fbffdfb7a3d27ab8a6874d2875b2b806f2
Chent Mokeev <chint2184@gmail.com>
2022-01-24 17:06:40 +0600

    fixed confirm of password

--- main.dart
+++ main.dart
@@ -5,13 +5,17 @@ import 'signIn.dart';
 import 'task2_User.dart';
 
 void main() {
+  print('Введите логин: ');
   var login = stdin.readLineSync()!;
+  print('Введите пароль: ');
   var password = stdin.readLineSync()!;
+  print('Повторите пароль: ');
+  var confirmPassword = stdin.readLineSync()!;
 
   var ivan = new User(login: login, password: password);
 
   try {
-    final sign = signIn(ivan.login, ivan.password, "qwerty");
+    final sign = signIn(ivan.login, ivan.password, confirmPassword);
     print('logged in');
   } on WrongLoginException catch (e) {
     print(e);
